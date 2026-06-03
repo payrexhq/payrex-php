a52f48a763f15c16ebf3e72156f748acd89470b0
Jaime Hing III <jaimehing@gmail.com>
2026-06-03 19:35:52 +0800

    Replace customer_Id with customer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.8.7] - 2026-06-03
+
+Utilize customer instead of customer_id in customer sessions.
+
 ## [1.8.6] - 2026-05-30
 
 - Add attach payment method to payment intent endpoint
--- src/Entities/CustomerSession.php
+++ src/Entities/CustomerSession.php
@@ -5,7 +5,7 @@ namespace Payrex\Entities;
 class CustomerSession extends \Payrex\Entities\BaseEntity
 {
     public $id;
-    public $customer_id;
+    public $customer;
     public $client_secret;
     public $livemode;
     public $components;
@@ -19,7 +19,7 @@ class CustomerSession extends \Payrex\Entities\BaseEntity
         $data = $apiResource->data;
 
         $this->id = $data['id'];
-        $this->customer_id = $data['customer_id'];
+        $this->customer = $data['customer'];
         $this->client_secret = $data['client_secret'];
         $this->livemode = $data['livemode'];
         $this->components = $data['components'];
